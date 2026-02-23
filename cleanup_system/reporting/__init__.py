32c3e1eb382d4e932dcd64f9ce91d143e7af1ffc
Martin Lukac <m.b.lukac@gmail.com>
2026-02-23 08:31:01 +0100

    chore: Remove cleanup_system

--- cleanup_system/reporting/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Progress tracking and reporting components."""
-
-from .progress import ProgressTracker
-from .reports import ReportGenerator
-
-__all__ = [
-    "ProgressTracker",
-    "ReportGenerator",
-]
